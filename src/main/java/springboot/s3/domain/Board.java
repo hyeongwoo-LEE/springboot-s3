b9828ed275b6c0837b87aadb3eb4c06e31d0470c
hyeongwoo-LEE <lhw970804@naver.com>
2021-12-28 20:05:57 +0900

    board 엔티티생성

--- src/main/java/springboot/s3/domain/Board.java
+++ src/main/java/springboot/s3/domain/Board.java
@@ -1,4 +1,36 @@
 package springboot.s3.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity
 public class Board {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Builder.Default
+    private String text = "";
+
+    @Builder.Default
+    private String folderPath = "";
+
+    @Builder.Default
+    private String filename = "";
+
 }
